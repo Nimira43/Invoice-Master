605d3e4666cc30d1ff8252c23e589867c64e651d
Nimira43 <n.rabone72@aol.com>
2024-06-11 21:13:51 +0100

    Last commit today.

--- js/positionElements.js
+++ js/positionElements.js
@@ -6,6 +6,9 @@ class PositionElements {
     this.leftPositions = [0, 8, 16, 24, 32]
     this.topPositions = [0, 6, 12, 18]
   }
+  shuffle(array) {
+    return array.sort(() => Math.random() - 0.5)
+  }
 }
 
 export default PositionElements
