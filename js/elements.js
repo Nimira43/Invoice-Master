d1bcf995c4cf0a8cb0c59ed37b483a35b870630d
Nimira43 <n.rabone72@aol.com>
2024-06-11 20:41:44 +0100

    Developing Elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,6 +1,14 @@
 class Elements {
   constructor() {
-
+    this.puzzle = document.querySelector('.puzzle')
+    this.cellsAmount = 20
+    this.puzzleDivs = []
+    this.draggableDivs = []
+  }
+  createElements() {
+    for (let index = 0; index < this.cellsAmount; index++) {
+      
+    }
   }
 }
 
