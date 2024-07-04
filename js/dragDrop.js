c4060037972feff379c7cec270e59a2d79c6321a
Nimira43 <n.rabone72@aol.com>
2024-07-04 08:42:52 +0100

    Rebooted repo - will do a project about a French Winehouse

--- js/dragDrop.js
+++
@@ -1,9 +0,0 @@
-import PositionElements from "./positionElements.js"
-
-class DragDrop {
-  constructor() {
-    this.positionElements = new PositionElements()
-  }
-}
-
-export default DragDrop
--- script.js
+++
@@ -1,3 +0,0 @@
-import DragDrop from "./js/dragDrop.js"
-
-new DragDrop()
